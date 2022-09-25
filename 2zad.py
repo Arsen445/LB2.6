1f77f53e839cb4135c85be1249b50440c17e0215
Arsen445 <arsen454545454@gmail.com>
2022-09-26 01:53:10 +0300

    new4

--- 2zad.py
+++ 2zad.py
@@ -3,10 +3,9 @@
 
 
 if __name__ == '__main__':
-    slov = {1:"sid", 2:"Mani", 3:"Shiba"}
+    slov = {1: "sid", 2: "Mani", 3: "Shiba"}
     print (slov)
     nn = {}
-    for k, v in slov.items(): 
-       nn[v]=k
+    for k, v in slov.items():
+        nn[v] = k
     print (nn)
-        
