981ed956ef2be6c0357a089e68ce7e63a6db1e22
Arsen445 <arsen454545454@gmail.com>
2022-09-25 22:27:09 +0300

    new

---
+++ 1zad.py
@@ -0,0 +1,10 @@
+school = {"1а": 8, "1б": 12, "2б": 12, "6а": 4, "7в": 19}
+print (school, "количество учащихся в разных классах")
+school['1а'] = 22
+print (school, "1 изменение")
+school['1г'] = 26
+print (school, "новый класс")
+del school["2б"]
+print (school, "класс расформирован")
+k = sum(school.values())
+print(k, "кол-во учеников")
